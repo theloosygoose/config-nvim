83dcee7965aa5a0c85a22174980fb8313d1c4e01
theloosygoose <g.ramahenry2439@gmail.com>
2024-03-20 14:40:30 -0400

    formatting git git diffs cleaning from pull;

--- after/ftplugin/gdscript.vim
+++ after/ftplugin/gdscript.vim
@@ -1,9 +1,6 @@
-<<<<<<< Updated upstream
-"setlocal foldmethod=expr
-=======
+
 let g:godot_executable = '/mnt/c/Users/thelo/Non_Registry_Programs/Godot/4.2.1/godot.exe'
 
->>>>>>> Stashed changes
 setlocal shiftwidth=4
 setlocal tabstop=4
 
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,8 @@
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+vim.opt.numberwidth = 5
+vim.opt.columns = 85
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -13,10 +15,10 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undofile = true 
+vim.opt.undofile = true
 
 vim.opt.hlsearch = false
-vim.opt.incsearch = true 
+vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
@@ -24,4 +26,3 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 
 vim.opt.clipboard = 'unnamedplus'
-
