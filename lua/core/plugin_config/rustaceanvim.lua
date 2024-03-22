a252ac2934d58c9a8340fb18f1d95d54648d4449
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2024-03-21 20:59:38 -0400

    more stuff for rustaceanvim

--- lua/core/plugin_config/rustaceanvim.lua
+++ lua/core/plugin_config/rustaceanvim.lua
@@ -2,6 +2,7 @@ vim.g.rustaceanvim = {
     tools = {
         -- ...
     },
+
     server = {
         on_attach = function(_, bufnr)
             -- Set keybindings, etc. here.
@@ -11,7 +12,9 @@ vim.g.rustaceanvim = {
             vim.keymap.set("n", "<leader>dr", function()
                 vim.cmd.RustLsp("debuggables")
             end, { desc = "Rust debuggables", buffer = bufnr })
+
         end,
+
         default_settings = {
             -- rust-analyzer language server configuration
             ['rust-analyzer'] = {
@@ -33,3 +36,27 @@ vim.g.rustaceanvim = {
         -- ...
     },
 }
+
+vim.g.rustaceanvim = function()
+  -- Update this path
+  local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/'
+  local codelldb_path = extension_path .. 'adapter/codelldb'
+  local liblldb_path = extension_path .. 'lldb/lib/liblldb'
+  local this_os = vim.uv.os_uname().sysname;
+
+  -- The path is different on Windows
+  if this_os:find "Windows" then
+    codelldb_path = extension_path .. "adapter\\codelldb.exe"
+    liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+  else
+    -- The liblldb extension is .so for Linux and .dylib for MacOS
+    liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+  end
+
+  local cfg = require('rustaceanvim.config')
+  return {
+    dap = {
+      adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+    },
+  }
+end
