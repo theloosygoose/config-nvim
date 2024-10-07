16c09170ef442f6d30b7cc6b6a8f0e24066000d7
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2024-10-07 00:38:20 -0400

    markdown filetype options

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,11 @@
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+
+-- Line Thinkgs
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.linespace = 20
+vim.opt.textwidth = 80
--- after/ftplugin/tex.vim
+++ after/ftplugin/tex.vim
@@ -4,4 +4,4 @@ let g:vimtex_view_method = 'zathura'
 
 let g:vimtex_compiler_method = 'latexmk'
 
-let maplocalleader = "["
+let maplocalleader = ","
