835485251500e910041dcf90dc41db962e290d6b
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2024-10-07 00:41:41 -0400

    markdown filetype linewrapping keymaps

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -9,3 +9,11 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.linespace = 20
 vim.opt.textwidth = 80
+
+-- navigating wrapped lines
+vim.keymap.set('n', "j", "gj")
+vim.keymap.set('n', "k", "gk")
+
+vim.keymap.set('v', "j", "gj")
+vim.keymap.set('v', "k", "gk")
+
